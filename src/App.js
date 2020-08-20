d2ee7fd63ba800f41178fadc65bab9131cd77ebc
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-08-20 14:45:36 +0900

    numberを登録できるようにした

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas' } ]);
+  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas', number: '1234-52121' } ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNomber, setNewNomber ] = useState('');
   const handleNmaeChange = (event) => {
     console.log('change', event.target.value);
     setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    console.log('change', event.target.value);
+    setNewNomber(event.target.value);
   };
 
   const handleSetPerson = (event) => {
@@ -16,7 +21,8 @@ const App = () => {
       return;
     }
     const newUser = {
-      name: newName
+      name: newName,
+      number: newNomber
     };
     setPersons(persons.concat(newUser));
     setNewName('');
@@ -30,12 +36,21 @@ const App = () => {
           name: <input value={newName} onChange={handleNmaeChange} />
         </div>
         <div>
+          number: <input value={newNomber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       ...
-      <div>debug:{persons.map((person, index) => <li key={index}>{person.name}</li>)}</div>
+      <div>
+        debug:{persons.map((person, index) => (
+          <li key={index}>
+            {person.name}: {person.number}
+          </li>
+        ))}
+      </div>
     </div>
   );
 };
